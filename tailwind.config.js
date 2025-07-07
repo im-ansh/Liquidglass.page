627cd3c2020b641b019f482f69f973d25bade611
Ansh Bhardwaj <aaronvanoss@gmail.com>
2025-07-07 22:26:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      dropShadow: {
+        glass: "0 4px 30px rgba(0, 0, 0, 0.1)",
+      },
+    },
+  },
+  plugins: [],
+};
